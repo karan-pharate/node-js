4e1f1fff9b700e40dcd416a668802745eff6102b
karan-pharate <karan.pharate@codeastu.com>
2019-09-23 18:38:52 +0530

    adds method to open a file

--- event-loop/main.js
+++ event-loop/main.js
@@ -1,14 +1,13 @@
-var events = require('events');
+var events = require("events");
 var eventEmitter = new events.EventEmitter();
 var connectHandler = function conneted() {
-    console.log('connection succesful');
-    eventEmitter.emit('data_received');
-}
-eventEmitter.on('connection',connectHandler);
+  console.log("connection succesful");
+  eventEmitter.emit("data_received");
+};
+eventEmitter.on("connection", connectHandler);
 
-eventEmitter.on('data_received', function() {
-    console.log('data received successfully.');
+eventEmitter.on("data_received", function() {
+  console.log("data received successfully.");
 });
-eventEmitter.emit('connection');
-console.log('Program ended.')
-
+eventEmitter.emit("connection");
+console.log("Program ended.");
