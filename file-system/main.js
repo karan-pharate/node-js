0bee91a0f4d567c548dbb20c0fa0ace00e589f57
karan-pharate <karan.pharate@codeastu.com>
2019-09-23 18:53:05 +0530

    adds method to get file information

--- file-system/main.js
+++ file-system/main.js
@@ -18,3 +18,20 @@ fs.stat("input.txt", function(err, stats) {
   console.log("isFile ? " + stats.isFile());
   console.log("isDirectory ? " + stats.isDirectory());
 });
+//writing a file
+console.log("Going to write into existing file");
+fs.writeFile("input.txt", "Simply Easy Learning!", function(err) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log("Data written successfully!");
+  console.log("Let's read newly written data");
+
+  fs.readFile("input.txt", function(err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log("Asynchronous read: " + data.toString());
+  });
+});
