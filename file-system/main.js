8c1436ad72a59f46576e13321933b3fe9c5ee802
karan-pharate <karan.pharate@codeastu.com>
2019-09-23 18:52:19 +0530

    adds method to get file information

--- file-system/main.js
+++ file-system/main.js
@@ -6,3 +6,15 @@ fs.open("input.txt", "r+", function(err, fd) {
   }
   console.log("file opened successfully");
 });
+// Check file type
+console.log("Going to get file info!");
+fs.stat("input.txt", function(err, stats) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(stats);
+  console.log("Got file info successfully!");
+
+  console.log("isFile ? " + stats.isFile());
+  console.log("isDirectory ? " + stats.isDirectory());
+});
