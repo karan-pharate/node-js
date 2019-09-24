0d8fac155eb3ef81eeb2dbcbe3429cd11a07e9b7
karan-pharate <karan.pharate@codeastu.com>
2019-09-24 18:57:16 +0530

    code formatting

--- Assignments/readfile/main.js
+++ Assignments/readfile/main.js
@@ -1,16 +1,15 @@
-var fs = require('fs');
+var fs = require("fs");
 
-fs.access('something.txt',fs.F_OK, (err) => {
+fs.access("something.txt", fs.F_OK, err => {
+  if (err) {
+    console.log(`Error occured while checking permissions ${err.message}`);
+    return;
+  }
+  fs.readFile("something.txt", (err, data) => {
     if (err) {
-       console.log(`Error occured while checking permissions ${err.message}`);
-       return;
+      console.log(`Error occured while reading file ${err.message}`);
+      return;
     }
-    fs.readFile('something.txt',(err, data) => {
-        if (err) {
-            console.log(`Error occured while reading file ${err.message}`);
-            return;
-        }
-        console.log(`Contents: ${data.toString()}`);
-    })
-})
-
+    console.log(`Contents: ${data.toString()}`);
+  });
+});
